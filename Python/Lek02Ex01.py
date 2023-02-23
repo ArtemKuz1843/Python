9d26e86e272d332bc9864c628a6d0191ce6d118d
ArtemKuz <KristofRamuald-1843@yandex.ru>
2023-02-23 14:08:37 +0300

    Дексия 2 0,5

---
+++ Python/Lek02Ex01.py
@@ -0,0 +1,45 @@
+# list_1 = [] # Создание пустого списка
+# list_1 = list() # Создание пустого списка
+# list_1 = [7, 9, 11, 13, 15, 17]
+# print (*list_1) # символ * убирает скобки
+
+# for i in list_1:
+#     print (i)
+
+# print (len(list_1)) # len -длина
+
+# print (list_1[0])
+# print (list_1[3])
+# print (list_1[-1])
+# print (list_1[-2])
+
+# list_1 = [1, 5]
+# print(list_1)
+# list_1.append(8) # append - добавит элемент в конец
+# print(list_1)
+
+# list_1 = []
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+
+# list_1 = [12, 7, -1, 21, 0] # удаление последнего элемента списка
+# print(list_1.pop()) # 0 удалит последнее значение и возвращает его
+# print(list_1) # [12, 7, -1, 21]
+# print(list_1.pop()) # 21
+# print(list_1) # [12, 7, -1]
+# print(list_1.pop()) # -1
+# print(list_1) # [12, 7]
+#
+# a = list_1.pop()
+# print(a)
+
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop(1)) # удаляет элемент из списка по индексу и возвращает его
+# print(list_1)
+
+
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.insert(2, 11)) # добавление элемента на нужную позицию
+# print(list_1) # [12, 7, 11, -1, 21, 0]
