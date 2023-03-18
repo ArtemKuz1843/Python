0fc856a6ef47d406198367ad11a4ae91bad43f35
ArtemKuz <KristofRamuald-1843@yandex.ru>
2023-03-18 21:36:14 +0300

    Лекция 2

--- Python/Lek02Ex01.py
+++ Python/Lek02Ex01.py
@@ -43,3 +43,113 @@
 # list_1 = [12, 7, -1, 21, 0]
 # print(list_1.insert(2, 11)) # добавление элемента на нужную позицию
 # print(list_1) # [12, 7, 11, -1, 21, 0]
+
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[0]) # 1
+# print(list_1[1]) # 2
+# print(list_1[len(list_1)-1]) # 10
+# print(list_1[-5]) # 6
+# print(list_1[:]) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[:2]) # [1, 2]
+# print(list_1[len(list_1)-2:]) #[9, 10]
+# print(list_1[2:9]) # [3, 4, 5, 6, 7, 8, 9]
+# print(list_1[6:-18]) # []
+# print(list_1[0:len(list_1):6]) # [1, 7]
+# print(list_1[::6]) # [1, 7]
+
+# t = () # создание пустого кортежа
+# print(type(t)) # class <'tuple'>
+# t = (1) # не кортеж а int
+# print(type(t))
+# t = (1,) # с "," - уже кортеж, должна быть в конце!!!
+# print(type(t))
+# t = (28, 9, 1990) # с несколькими числами почему-то круглые скобки делают кортеж
+# print(type(t))
+# t = (28, 9, 1990,)
+# print(type(t))
+
+# t = [28, 9, 1990]
+# print(type(t))
+# t = tuple(t)
+# print(type(t))
+# a, b, c = t
+# print(a, b, c)
+
+# t = (28, 9, 1990)
+# for i in range(len(t)):
+#     print(t[i])
+# t[0] = 0 # в кортеже нельзя преобразовывать элементы TypeError: 'tuple' object does not support item assignment
+
+# colors = ['red', 'green', 'blue']
+# print(colors) # ['red', 'green', 'blue']
+# t = tuple(colors)
+# print(t) # ('red', 'green', 'blue')
+# # t = tuple(['red', 'green', 'blue'])
+# print(t[0]) # red
+# print(t[2]) # blue
+# for e in t:
+#     print(e) # red green blue
+# t[0] = 'black' # TypeError: 'tuple' object does not support(нельзя изменятькортеж)
+
+# dictionary = {}
+# dictionary ={'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+# print(dictionary['left']) # ←
+# # типы ключей могут отличаться
+# print(dictionary['up']) # ↑
+# dictionary[895] = 98998
+# print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→', 895: 98998}
+# # типы ключей могут отличаться
+# dictionary['left'] = '⇐'
+# print(dictionary['left']) # ⇐
+# # print(dictionary['type']) # KeyError: 'type' не существует такого ключа
+# del dictionary['left'] # удаление элемента
+# print(dictionary)
+# for item in dictionary: # for (k,v) in dictionary.items():
+#     print('{}: {}'.format(item, dictionary[item]))
+#
+# for (k,v) in dictionary.items():
+#     print(k, v)
+# # up: ↑
+# # down: ↓
+# # right: →
+# # 895 98998
+
+# colors = {'red', 'green', 'blue'}
+# print(colors) # {'red', 'green', 'blue'}
+# colors.add('red')
+# print(colors) # {'red', 'green', 'blue'}
+# colors.add('gray')
+# print(colors) # {'red', 'green', 'blue','gray'}
+# colors.remove('red')
+# print(colors) # {'green', 'blue','gray'}
+# # colors.remove('red') # KeyError: 'red' вызывает ошибку
+# colors.discard('red') # ok а discard не вызывает
+# print(colors) # {'green', 'blue','gray'}
+# colors.clear() # { }
+# print(colors) # set()
+# # q = set() # создаст пустое множество
+
+# a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy() # c = {1, 2, 3, 5, 8}
+# u = a.union(b) # u = {1, 2, 3, 5, 8, 13,
+# i = a.intersection(b) # i = {8, 2, 5}
+# dl = a.difference(b) # dl = {1, 3}
+# dr = b.difference(a) # dr = {13, 21}
+# q=a.union(b).difference(a.intersection(b)) # {1, 21, 3, 13}
+
+# a = {1, 2, 3, 5, 8}
+# b = frozenset(a)
+# print(b) # frozenset({1, 2, 3, 5, 8})
+
+list_1 = []
+for i in range(1, 101):
+    list_1.append(i)
+print(list_1) # [1, 2, 3,..., 100]
+
+list_1 = [i for i in range(1, 101)] # [1, 2, 3,..., 100]
+list_1 = [i for i in range(1, 101) if i % 2 == 0] # [2, 4, 6,..., 100]
+list_1 = [(i, i) for i in range(1, 101) if i % 2 == 0] # [(2, 2), (4, 4),...,(100, 100)]
+list_1 = [i * 2 for i in range(10) if i % 2 == 0]
+print(list_1) # [0, 4, 8, 12, 16]
---
+++ Python/Sem03Ex01.py
@@ -0,0 +1,75 @@
+# k = int(input('Укажите сдвиг'))
+# values = [1, 2, 3, 4, 5]
+# length = len(values)
+# k = k % length
+# print(values)
+# print(values[-k:] + values[:-k])
+
+
+# dictionary = [{"V": "S001"}, {"V":"S002"}, {"VI":"S001"}, {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
+# unique = {''}
+# unique.clear()
+# for item in dictionary:
+#     print(item)
+#     for temp in item:
+#         unique.add(f'{item[temp]}')
+# print(unique)
+
+
+# source = [0, -1, 5, 2, 3, 5]
+#
+# count = 0
+# idx = 0
+# while idx < len(source) - 1:
+#     idx += 1
+#     if source[idx - 1] < source[idx]:
+#         count += 1
+#
+# print(count)
+
+# Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ уже встречался. Количество повторов добавляется к символам с помощью постфикса формата _n.
+# Пример ввода:
+# a a a b c a a d c d d
+#
+# Пример вывода:
+# a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+#
+
+
+# str1 = "a a a b c a a d c d d"
+#
+# def twin_counter(str1):
+#     str1 = str1.split(" ")
+#     temp = []
+#
+#     for i in range(len(str1)):
+#         counter = 0
+#         for j in str1[:i]:
+#             if str1[i] == j:
+#                 counter += 1
+#         if counter > 0:
+#             counter = f'_{counter}'
+#         else:
+#             counter = ''
+#
+#         temp.append(f"{str1[i]}{counter}")
+#
+#     print(temp)
+
+# twin_counter(str1)
+
+
+# source = 'a a a b c a a d c d d'.split()
+# count_dict = {}
+#
+# def transfer(source_list: list):
+#     # for idx, el in enumerate(source_list, start=1):
+#     for el in source_list:
+#         if el not in count_dict:
+#             count_dict[el] = 0
+#             yield el
+#             continue
+#         count_dict[el] += 1
+#         yield f'{el}_{count_dict[el]}'
+#
+# [print(_, end=' ') for _ in transfer(source)]
