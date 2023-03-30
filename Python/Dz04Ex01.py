04900c786b3600569ef33ade5cf8b611b1b33a7e
ArtemKuz <KristofRamuald-1843@yandex.ru>
2023-03-31 01:29:54 +0300

    Дз04

--- Python/Dz04Ex01.py
+++ Python/Dz04Ex01.py
@@ -2,17 +2,22 @@
 # возрастания все те числа, которые встречаются в обоих наборах. Пользователь вводит 2 числа. n - кол-во элементов
 # первого множества. m - кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 
-size1 = int(input('Введите раазмер первого набора: '))
-a = []
-print(type(a))
-print(a)
-# for i in set1:
-#     # i = int(i)
-#     i = int(input('Введите число в набор: '))
-#     # set1.append(i)
-# print(sеt1)
+n = int(input('Введите раазмер первого набора n: '))
+kit1 = []
+for i in range(n):
+    i = int(input('Введите число в набор n: '))
+    kit1.append(i)
+print(kit1)
+kit1 = set(kit1)
 
-# size2 = int(input('Введите раазмер первого набора: '))
-# sеt2 = []*size2
-# print(sеt2)
+m = int(input('Введите раазмер второго набора m: '))
+kit2 = []
+for i in range(m):
+    i = int(input('Введите число в набор m: '))
+    kit2.append(i)
+print(kit2)
+kit2 = set(kit2)
 
+kit3 = kit1.intersection(kit2)
+kit3 = sorted(list(kit3))
+print(f'Пересечение наборов и сортировка: {kit3}')
